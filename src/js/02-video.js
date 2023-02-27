9f6199caf2c5778770bd93d712046e8e7badbfa9
Vas2306 <vasilisa230679@gmail.com>
2023-02-27 23:45:40 +0100

    fixing setCurrentTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,20 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = "videoplayer-current-time";
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
+const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay(seconds) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+   
+function onPlay ({ seconds }) {
+    localStorage.setItem(CURRENT_TIME, seconds)
 }
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+setCurrentTime()
+function setCurrentTime(){
+    if(!localStorage.getItem(CURRENT_TIME)){
+        return
+    }
+    player.setCurrentTime(localStorage.getItem(CURRENT_TIME))
+}
 
