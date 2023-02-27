43a37a5ea59ed01d00f967a7b900863b5e6572f3
Vas2306 <vasilisa230679@gmail.com>
2023-02-27 16:38:26 +0100

    second task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const formData = {};
+
+function onFormData(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(evt) {
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const email = document.querySelector(STORAGE_KEY);
+  const message = document.querySelector(STORAGE_KEY);
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+}
 
