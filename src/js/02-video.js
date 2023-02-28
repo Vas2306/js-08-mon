2ea0aaa21c7914f49d09a5e227d0f44e5c7582e7
Vas2306 <vasilisa230679@gmail.com>
2023-02-28 11:02:16 +0100

    fixing bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,7 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
    
-function onPlay ({ seconds }) {
+function onPlay (seconds) {
     localStorage.setItem(CURRENT_TIME, seconds)
 }
 setCurrentTime()
